8a16dbe23cfff5e1d05b346d0676b286801b9f5a
Ricardo González Moreno <ricardo@richiware.dev>
2022-01-31 16:19:13 +0100

    Refs #12820. Add DomainParticipantQos::name
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- fastdds_python/test/api/test_qos.py
+++ fastdds_python/test/api/test_qos.py
@@ -1,4 +1,20 @@
 import fastdds
 
 def test_participant_qos():
-    assert 1 == 0
+    # DomainParticipantQos
+    participant_qos = fastdds.DomainParticipantQos()
+
+    # .name
+    participant_qos.name("test name")
+    assert("test name" == participant_qos.name())
+
+    factory = fastdds.DomainParticipantFactory.get_instance()
+    factory.set_default_participant_qos(participant_qos)
+
+    default_participant_qos = fastdds.DomainParticipantQos()
+    factory.get_default_participant_qos(default_participant_qos)
+
+    print('adios {}'.format(participant_qos.name()))
+    print('hola {}'.format(default_participant_qos.name()))
+
+    assert("test name" == default_participant_qos.name())
