aeb9c23d896a6345fdc33782c59bc07d8d8ffe45
Ricardo González Moreno <ricardo@richiware.dev>
2022-03-09 14:28:31 +0100

    Refs #13384. Add DataReader.get_liveliness_changed_status
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- fastdds_python/test/api/test_datareader.py
+++ fastdds_python/test/api/test_datareader.py
@@ -497,6 +497,48 @@ def test_get_set_listener():
            factory.delete_participant(participant))
 
 
+def test_get_liveliness_changed_status():
+    """
+    This test checks:
+    - DataReader::get_liveliness_changed_status
+    """
+    factory = fastdds.DomainParticipantFactory.get_instance()
+    assert(factory is not None)
+    participant = factory.create_participant(
+            0, fastdds.PARTICIPANT_QOS_DEFAULT)
+    assert(participant is not None)
+    subscriber = participant.create_subscriber(fastdds.SUBSCRIBER_QOS_DEFAULT)
+    assert(subscriber is not None)
+    test_type = fastdds.TypeSupport(
+            test_complete.KeyedCompleteTestTypePubSubType())
+    assert(fastdds.ReturnCode_t.RETCODE_OK ==
+           participant.register_type(test_type, test_type.get_type_name()))
+    topic = participant.create_topic(
+            "Complete", test_type.get_type_name(), fastdds.TOPIC_QOS_DEFAULT)
+    assert(topic is not None)
+    datareader = subscriber.create_datareader(
+            topic, fastdds.DATAREADER_QOS_DEFAULT)
+    assert(datareader is not None)
+
+    status = fastdds.LivelinessChangedStatus()
+    assert(fastdds.ReturnCode_t.RETCODE_OK ==
+           datareader.get_liveliness_changed_status(status))
+    assert(0 == status.alive_count)
+    assert(0 == status.alive_count_change)
+    assert(0 == status.not_alive_count)
+    assert(0 == status.not_alive_count_change)
+    assert(fastdds.c_InstanceHandle_Unknown == status.last_publication_handle)
+
+    assert(fastdds.ReturnCode_t.RETCODE_OK ==
+           subscriber.delete_datareader(datareader))
+    assert(fastdds.ReturnCode_t.RETCODE_OK ==
+           participant.delete_topic(topic))
+    assert(fastdds.ReturnCode_t.RETCODE_OK ==
+           participant.delete_subscriber(subscriber))
+    assert(fastdds.ReturnCode_t.RETCODE_OK ==
+           factory.delete_participant(participant))
+
+
 def test_get_topicdescription():
     """
     This test checks:
