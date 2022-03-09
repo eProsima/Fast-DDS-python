41245ba0905c3a7d41dda27e3ba951d0cec2bb45
Ricardo González Moreno <ricardo@richiware.dev>
2022-03-09 14:34:42 +0100

    Refs #13384. Add DataReader::get_subscription_matched_status
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- fastdds_python/test/api/test_datareader.py
+++ fastdds_python/test/api/test_datareader.py
@@ -617,6 +617,48 @@ def test_get_sample_rejected_status():
            factory.delete_participant(participant))
 
 
+def test_get_subscription_matched_status():
+    """
+    This test checks:
+    - DataReader::get_subscription_matched_status
+    """
+    factory = fastdds.DomainParticipantFactory.get_instance()
+    assert(factory is not None)
+    participant = factory.create_participant(
+            0, fastdds.PARTICIPANT_QOS_DEFAULT)
+    assert(participant is not None)
+    subscriber = participant.create_subscriber(fastdds.SUBSCRIBER_QOS_DEFAULT)
+    assert(subscriber is not None)
+    test_type = fastdds.TypeSupport(
+            test_complete.KeyedCompleteTestTypePubSubType())
+    assert(fastdds.ReturnCode_t.RETCODE_OK ==
+           participant.register_type(test_type, test_type.get_type_name()))
+    topic = participant.create_topic(
+            "Complete", test_type.get_type_name(), fastdds.TOPIC_QOS_DEFAULT)
+    assert(topic is not None)
+    datareader = subscriber.create_datareader(
+            topic, fastdds.DATAREADER_QOS_DEFAULT)
+    assert(datareader is not None)
+
+    status = fastdds.SubscriptionMatchedStatus()
+    assert(fastdds.ReturnCode_t.RETCODE_OK ==
+           datareader.get_subscription_matched_status(status))
+    assert(0 == status.total_count)
+    assert(0 == status.total_count_change)
+    assert(0 == status.current_count)
+    assert(0 == status.current_count_change)
+    assert(fastdds.c_InstanceHandle_Unknown == status.last_publication_handle)
+
+    assert(fastdds.ReturnCode_t.RETCODE_OK ==
+           subscriber.delete_datareader(datareader))
+    assert(fastdds.ReturnCode_t.RETCODE_OK ==
+           participant.delete_topic(topic))
+    assert(fastdds.ReturnCode_t.RETCODE_OK ==
+           participant.delete_subscriber(subscriber))
+    assert(fastdds.ReturnCode_t.RETCODE_OK ==
+           factory.delete_participant(participant))
+
+
 def test_get_topicdescription():
     """
     This test checks:
@@ -1594,27 +1636,6 @@ def test_wait_for_unread_message():
 #     */
 #    RTPS_DllAPI ReturnCode_t get_requested_incompatible_qos_status(
 #            RequestedIncompatibleQosStatus& status);
-#
-#
-#    /**
-#     * @brief Get the SAMPLE_LOST communication status
-#     *
-#     * @param[out] status SampleLostStatus object where the status is returned.
-#     *
-#     * @return RETCODE_OK
-#     */
-#    RTPS_DllAPI ReturnCode_t get_sample_lost_status(
-#            SampleLostStatus& status) const;
-#
-#    /**
-#     * @brief Get the SAMPLE_REJECTED communication status
-#     *
-#     * @param[out] status SampleRejectedStatus object where the status is returned.
-#     *
-#     * @return RETCODE_OK
-#     */
-#    RTPS_DllAPI ReturnCode_t get_sample_rejected_status(
-#            SampleRejectedStatus& status) const;
 #
 #    /**
 #     * @brief Returns the subscription matched status
