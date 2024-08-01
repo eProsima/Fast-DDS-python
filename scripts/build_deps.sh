2d06beeb2be77abb8ed58dfe9f9b068e1719c8dc
Windel Bouwman <windel@windel.nl>
2024-08-01 23:12:24 +0200

    Add bash script to install and compile dependencies.

---
+++ scripts/build_deps.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Script to install required software in manylinux2014 container
+
+set -eu
+
+# Install tinyxml2 and asio
+yum install -y tinyxml2-devel asio-devel
+
+# Foo nathan
+git clone --branch v0.7-3 https://github.com/foonathan/memory.git
+mkdir memory/build
+pushd memory/build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DFOONATHAN_MEMORY_BUILD_EXAMPLES=OFF -DFOONATHAN_MEMORY_BUILD_TESTS=OFF
+make install
+popd
+
+# Fast CDR:
+git clone --branch 2.2.3 https://github.com/eProsima/Fast-CDR.git
+mkdir Fast-CDR/build
+pushd Fast-CDR/build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make install
+popd
+
+# Fast RTPS:
+git clone --branch v2.14.3 https://github.com/eProsima/Fast-DDS.git
+mkdir Fast-DDS/build
+pushd Fast-DDS/build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make install
+popd
